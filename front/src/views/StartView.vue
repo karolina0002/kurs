8d4cef273863baeed9e82df0780aa48f949d7c21
karolina0002 wapniarska <karolina.wapniarska@wp.pl>
2023-11-21 18:35:55 +0000

    init front

---
+++ front/src/views/StartView.vue
@@ -0,0 +1,23 @@
+<template>
+<p>start</p>
+</template>
+<script>
+export default {
+ data() {
+   return {
+     number: ''
+   }
+ },
+ methods: {
+   async call() {
+     await fetch(`${process.env.VUE_APP_SERVER_URL}/call`, {
+       method:  "POST",
+       headers: {
+         "Content-type" : "application/json; charset=UTF-8"
+       },
+       body: JSON.stringify({number:  this.number})
+     });
+   },
+ },
+}
+</script>
